e6983419b55da3569b4d73b543f29c0fd3983489
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-08-10 17:40:31 +0000

    added url for all the .html files

--- app.py
+++ app.py
@@ -18,9 +18,33 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-
 def index():
     return render_template("index.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+@app.route("/reviews")
+def reviews():
+    return render_template("reviews.html")
+
+
+@app.route("/log_in")
+def log_in():
+    return render_template("login.html")
+
+
+@app.route("/log_out")
+def log_out():
+    return render_template("logout.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
