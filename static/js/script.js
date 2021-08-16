8a94f99dea17002fd8b9ee27b40f3aa315bf03e9
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-08-16 14:10:37 +0000

    Worked on the layout, added fade in on scroll code

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
   });
+
+// Copyright year
+$('#copyright').text(new Date().getFullYear());
+
+
+// Fade in elements on sroll 
+// Code by https://vincoding.com/fade-in-elements-scroll-jquery/
+
+$(document).ready(function(){
+  $(window).scroll(function() {
+    $('.fadein').each(function() {
+
+        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        if (bottom_of_window > bottom_of_element) {
+          $(this).animate({'opacity':'1'}, 1000);
+        }
+    })
+  });
+});
