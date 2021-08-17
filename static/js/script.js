72a238a100a21c9a469a498cf0207c81d6d780c4
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-08-17 17:16:57 +0000

    changed the sidenav to appear on the left, added jQuery for fade in on scroll

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
+    $('.sidenav').sidenav({edge: "left"});
   });
 
 // Copyright year
@@ -13,12 +13,26 @@ $(document).ready(function(){
   $(window).scroll(function() {
     $('.fadein').each(function() {
 
-        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        var bottomOfElement = $(this).offset().top + $(this).outerHeight();
+        var bottomOfWindow = $(window).scrollTop() + $(window).height();
 
-        if (bottom_of_window > bottom_of_element) {
+        if (bottomOfWindow > bottomOfElement) {
           $(this).animate({'opacity':'1'}, 1000);
         }
     })
+  });
+});
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    $('.fadeinleft').each(function() {
+
+      var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window =  $(window).scrollTop() + $(window).height();
+
+      if (bottom_of_window > bottom_of_element) {
+        $(this).animate({'opacity':'1', 'margin-left':'0px'}, 1000);
+      }
+    });
   });
 });
