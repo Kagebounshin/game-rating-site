2fbe75e49efbfab5399da49610b78be376ce1ec3
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-09-05 12:44:26 +0000

    Cleaned and restructured the code

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-    $('.sidenav').sidenav({edge: "left"});
+  $('.sidenav').sidenav({edge: "left"});
 
-    // Copyright year
-    $('.copyright').text(new Date().getFullYear());
+  // Copyright year
+  $('.copyright').text(new Date().getFullYear());
 
-    $('.fadeinleftstart').each(function() {
-      $(this).animate({'opacity':'1', 'margin-left':'0px'}, 1000);
-    });
+  $('.fadeinleftstart').each(function() {
+    $(this).animate({'opacity':'1', 'margin-left':'0px'}, 1000);
+  });
 
-    // Fade in elements on sroll 
-    // Code by https://vincoding.com/fade-in-elements-scroll-jquery/
+  // Fade in elements on sroll 
+  // Code by https://vincoding.com/fade-in-elements-scroll-jquery/
   $(window).scroll(function() {
     $('.fadein').each(function() {
   
