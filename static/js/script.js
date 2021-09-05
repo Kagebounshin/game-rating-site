847a9f171f7527821d424112340570e245977027
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-09-05 12:44:12 +0000

    Cleaned and restructured the code

--- static/js/script.js
+++ static/js/script.js
@@ -1,39 +1,35 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "left"});
-  });
 
-// Copyright year
-$(document).ready(function(){
-  $('.copyright').text(new Date().getFullYear());
-});
+    // Copyright year
+    $('.copyright').text(new Date().getFullYear());
 
-// Fade in elements on sroll 
-// Code by https://vincoding.com/fade-in-elements-scroll-jquery/
+    $('.fadeinleftstart').each(function() {
+      $(this).animate({'opacity':'1', 'margin-left':'0px'}, 1000);
+    });
 
-$(document).ready(function(){
+    // Fade in elements on sroll 
+    // Code by https://vincoding.com/fade-in-elements-scroll-jquery/
   $(window).scroll(function() {
     $('.fadein').each(function() {
+  
+      var bottomOfElement = $(this).offset().top + $(this).outerHeight();
+      var bottomOfWindow = $(window).scrollTop() + $(window).height();
+  
+      if (bottomOfWindow > bottomOfElement) {
+        $(this).animate({'opacity':'1'}, 1000);
+      }
+    });
 
-        var bottomOfElement = $(this).offset().top + $(this).outerHeight();
-        var bottomOfWindow = $(window).scrollTop() + $(window).height();
-
-        if (bottomOfWindow > bottomOfElement) {
-          $(this).animate({'opacity':'1'}, 1000);
-        }
-    })
-  });
-});
-
-$(document).ready(function() {
-  $(window).scroll(function() {
     $('.fadeinleft').each(function() {
 
       var bottom_of_element = $(this).offset().top + $(this).outerHeight();
       var bottom_of_window =  $(window).scrollTop() + $(window).height();
-
+  
       if (bottom_of_window > bottom_of_element) {
         $(this).animate({'opacity':'1', 'margin-left':'0px'}, 1000);
       }
     });
   });
 });
+
