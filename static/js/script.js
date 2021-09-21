4625f2557541e01ce1e9a8f19125eb0228f14a06
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-09-21 16:06:06 +0000

    Added a function wich hides some of the review texts, just to keep the reviews looking consistance

--- static/js/script.js
+++ static/js/script.js
@@ -35,3 +35,31 @@ $(document).ready(function(){
   });
 });
 
+
+jQuery(function(){
+
+  var minimized_elements = $('p.minimize');
+  
+  minimized_elements.each(function(){    
+      var t = $(this).text();        
+      if(t.length < 100) return;
+      
+      $(this).html(
+          t.slice(0,100)+'<span>... </span><a class="more"> <span>|</span> Show More</a>'+
+          '<span style="display:none;">'+ t.slice(100,t.length)+' <a class="less"> <span>|</span> Show Less</a></span>'
+      );
+      
+  }); 
+  
+  $('a.more', minimized_elements).click(function(event){
+      event.preventDefault();
+      $(this).hide().prev().hide();
+      $(this).next().show();        
+  });
+  
+  $('a.less', minimized_elements).click(function(event){
+      event.preventDefault();
+      $(this).parent().hide().prev().show().prev().show();    
+  });
+
+});
