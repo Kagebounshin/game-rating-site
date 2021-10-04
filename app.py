5215cc4fdabca91714a82b1de75ecb3b03c4c2df
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-10-04 19:18:50 +0000

    Added a error 500 page

--- app.py
+++ app.py
@@ -258,5 +258,14 @@ def page_not_found(error):
     return render_template('404.html'), 404
 
 
+@app.errorhandler(500)
+def something_wrong(error):
+    """
+    This is the route to send 500 errors to the 500.html template.
+    """
+    return render_template('500.html'), 500
+
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
