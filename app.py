17656eec662cb0254284aebf7d3ce0ac5f0a30ab
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-10-20 10:17:07 +0000

    Debugger set to False

--- app.py
+++ app.py
@@ -361,4 +361,4 @@ def something_wrong(error):
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")), debug=True)
+            port=int(os.environ.get("PORT")), debug=False)
